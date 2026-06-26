7b519760066ebd123255b2b236a7e75c705a3cc4
lh1564803535-code <lh1564803535@gmail.com>
2026-06-26 17:16:04 +0800

    feat: Creator components barrel export index
    - Centralized exports for all creator components
    - Easier imports across the application
    
    TypeScript: zero errors

---
+++ src/components/creator/index.ts
@@ -0,0 +1,17 @@
+// Creator Components
+export { AchievementBadges } from "./AchievementBadges";
+export { AIAssistant } from "./AIAssistant";
+export { CampaignRecommendations } from "./CampaignRecommendations";
+export { CreatorAnalytics } from "./CreatorAnalytics";
+export { CreatorDashboard } from "./CreatorDashboard";
+export { CreatorProfile } from "./CreatorProfile";
+export { CreatorReputation } from "./CreatorReputation";
+export { CreatorStreak } from "./CreatorStreak";
+export { EarningsChart } from "./EarningsChart";
+export { LeaderboardTable } from "./LeaderboardTable";
+export { LiveEarnings } from "./LiveEarnings";
+export { PaymentStream } from "./PaymentStream";
+export { RewardAnimation } from "./RewardAnimation";
+export { RewardsBreakdown } from "./RewardsBreakdown";
+export { TransactionHistory } from "./TransactionHistory";
+export { YieldStaking } from "./YieldStaking";
